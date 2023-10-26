2252a4e13def5a3b31cbf1e77222bdf387ef4c27
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2023-10-27 00:37:36 +0300

    add

--- js/main.js
+++ js/main.js
@@ -9,14 +9,23 @@ $(window).scroll(function () {
 });
 $(".header-burger").click(function (event) {
   $(".header-nav").toggleClass("active");
+  $("body").toggleClass("lock");
 });
 $(".close-menu").click(function (event) {
   $(".header-nav").toggleClass("active");
+  $("body").toggleClass("lock");
 });
 $(".faq-item").click(function (event) {
-  $(this).closest("li").find(".answer").toggleClass("open");
-  $(this).closest("li").find(".plus").toggleClass("open");
+  var parentListItem = $(this).closest("li");
+  var isPlusOpen = parentListItem.find(".plus").hasClass("open");
+  $(".plus").removeClass("open");
+  $(".answer").removeClass("open");
+  if (isPlusOpen) {
+    parentListItem.find(".answer").removeClass("open");
+  } else {
+    parentListItem.find(".answer").addClass("open");
+    parentListItem.find(".plus").addClass("open");
+  }
 });
-Fancybox.bind("[data-fancybox]", {
-  // Your custom options
-});
+
+Fancybox.bind("[data-fancybox]", {});
